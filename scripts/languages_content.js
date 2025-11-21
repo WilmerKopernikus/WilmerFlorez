c4016db7cf2998dc4ae4e916212650b5715f82a1
Wilmer Kopernikus <wilmerkopernikus@gmail.com>
2025-11-21 10:55:54 +0800

    projekte.html renovado

--- scripts/languages_content.js
+++ scripts/languages_content.js
@@ -7,39 +7,32 @@ const languagesContent = {
     menuProjekte: `Projekte`,
     menuCV: `Lebenslauf`,
     menuKontakt: `Kontakt`,
+
     //Index
     indexTitle: `WILLKOMMEN IN EINEM <br> NEUEN DIGITALEN KOSMOS!`,
-    wilmerIntroduction: `Ich bin Webentwickler, Grafikdesigner und bildender Künstler mit internationaler 
-    Erfahrung in Deutschland, China und Kolumbien. In diesem Portfolio präsentiere ich sowohl berufliche 
-    als auch persönliche Projekte, die meinen Ansatz widerspiegeln: Design, Technologie und Kreativität 
-    zu verbinden, um digitale Lösungen auf höchstem Niveau zu entwickeln.`,
-
-    projectTitle: `PROJEKTE`,
-    projectContent: `Erfahren Sie in verschiedenen Projektbeispielen, wie ich Kunden mit wirkungsvollem Webdesign und -entwicklung unterstützt habe.`,
-    projectButton: `Zu meinen Projekten`,
-
+    wilmerIntroduction: "Ich bin Webentwickler, Grafikdesigner und bildender Künstler mit internationaler Erfahrung in Deutschland, China und Kolumbien. In diesem Portfolio präsentiere ich sowohl berufliche als auch persönliche Projekte, die meinen Ansatz widerspiegeln: Design, Technologie und Kreativität zu verbinden, um digitale Lösungen auf höchstem Niveau zu entwickeln.",
+    projectTitle: "PROJEKTE",
+    projectContent: "Erfahren Sie in verschiedenen Projektbeispielen, wie ich Kunden mit wirkungsvollem Webdesign und -entwicklung unterstützt habe.",
+    projectButton: "Zu meinen Projekten",
     curriculumTitle: `LEBENSLAUF`,
-    cVContent: `Entdecken Sie meinen Lebenslauf in digitaler Form und erfahren Sie mehr über meine 
-    berufliche Erfahrung, meine technischen Fähigkeiten und meinen internationalen Werdegang.`,
+    cVContent: "Entdecken Sie meinen Lebenslauf in digitaler Form und erfahren Sie mehr über meine berufliche Erfahrung, meine technischen Fähigkeiten und meinen internationalen Werdegang.",
     curriculumButton: `Zu meinem Lebenslauf`,
-
-    contactTitle: `KONTAKT`,
-    contactContent: `Möchten Sie mit mir zusammenarbeiten? Haben Sie bereits konkrete Vorstellungen oder 
-    möchten Sie gemeinsam erste Ideen entwickeln? Kontaktieren Sie mich gerne – und lassen Sie uns Ihre 
-    digitale Zukunft in Bewegung setzen.`,
+    contactTitle: "KONTAKT",
+    contactContent: "Möchten Sie mit mir zusammenarbeiten? Haben Sie bereits konkrete Vorstellungen oder möchten Sie gemeinsam erste Ideen entwickeln? Kontaktieren Sie mich gerne – und lassen Sie uns Ihre digitale Zukunft in Bewegung setzen.", 
     contactButton: `Zum Kontakt`,
 
     //Projekte
     projekteTitle: "PROJEKTE",
-
     joblabKunde: "<strong>Kunde:</strong><br /> Deutscher Akademischer Austauschdienst (DAAD)",
-    joblabLeistungen: "<strong>Unsere Leistungen:</strong><br/> Markenentwicklung<br/> Plakatgestaltung<br/> Webentwicklung<br/> Webdesign",
+    joblabLeistungen: "<strong>Unsere Leistungen:</strong><br/> Webentwicklung - Webdesign - Markenentwicklung - Plakatgestaltung",
     joblabUebersicht: "<strong>Übersicht:</strong><br/>Im Mai und Juni 2024 organisierte der DAAD in China das Projekt Job Lab in Zusammenarbeit mit der Deutschen Botschaft, dem Goethe-Institut und weiteren renommierten Institutionen. Unter dem Motto „Mit Deutsch in die Zukunft“ bot die Messe Karriereperspektiven für chinesische Germanistik-Studierende, um sie auf ihrem Weg in die Arbeitswelt zu unterstützen.",
-    
-    blancecKunde:"",
-    blancecLeistungen:"",
-    blancecUebersicht:"",
-
+    blancecKunde:"<strong>Kunde:</strong><br/> Laboratorios Blancec S.A.S.",
+    blancecLeistungen:"<strong>Unsere Leistungen:</strong><br /> Webentwicklung & Design <br /> Markenentwicklung <br /> Datenbank <br /> Prozessautomatisierung",
+    blancecUebersicht:"<strong>Übersicht:</strong><br />BLANCEC S.A.S. ist ein Labor aus Kolumbien, das sich auf Produkttests spezialisiert hat. Es bewertet Produkte aus verschiedenen Branchen, um sicherzustellen, dass sie die erforderlichen Vorschriften für die Vermarktung auf globalen Märkten erfüllen - und damit höchste Qualität, Sicherheit und Zuverlässigkeit gewährleisten.",
+    tarotTitle:"INTERACTIVES TAROT",
+    tarotKunde:"<strong>Kunde:</strong><br /> Nichtkommerzielles Projekt",
+    tarotLeistungen:"<strong>Unsere Leistungen:</strong><br /> Webentwicklung & dESIGN",
+    tarotUebersicht:"<strong>Übersicht:</strong><br />Kooperationsprojekt mit Professor Dioscórides Pérez, Professor für Bildende Kunst an der Nationalen Universität von Kolumbien, zur Entwicklung eines interaktiven Tarotspiels. Auf Grundlage seiner Originalzeichnungen wurde das digitale Programm entwickelt, das seine künstlerische und symbolische Vision in ein interaktives Erlebnis verwandelt.",
     mehrInformation: "Mehr Information",
     
     //Joblab
@@ -49,26 +42,9 @@ const languagesContent = {
     //Kontakt
     heroTitle: 'KONTAKT',
     chooseUsTitle: 'WÄHLEN SIE UNS!',
-    chooseUsText: `
-      Bereit Ihr Unternehmen auf das nächste Level zu bringen? 
-      Eine Website zu haben bedeutet nicht nur, online zu sein – es bedeutet, neue Chancen 
-      zu eröffnen. Eine gut gestaltete Website hilft Ihnen, Kunden zu gewinnen, Vertrauen 
-      aufzubauen und Ihre Dienstleistungen rund um die Uhr zu präsentieren.
-      <br><br>
-      Wir unterstützen Sie dabei, eine Website zu erstellen, die nicht nur visuell ansprechend, 
-      sondern auch benutzerfreundlich und suchmaschinenoptimiert ist. Gemeinsam mit Ihnen erarbeiten 
-      wir eine Lösung, die Ihre Unternehmensziele bestmöglich unterstützt.
-      <br><br>
-    `,
+    chooseUsText: "Bereit Ihr Unternehmen auf das nächste Level zu bringen? Eine Website zu haben bedeutet nicht nur, online zu sein – es bedeutet, neue Chancen zu eröffnen. Eine gut gestaltete Website hilft Ihnen, Kunden zu gewinnen, Vertrauen aufzubauen und Ihre Dienstleistungen rund um die Uhr zu präsentieren. <br><br>Wir unterstützen Sie dabei, eine Website zu erstellen, die nicht nur visuell ansprechend, sondern auch benutzerfreundlich und suchmaschinenoptimiert ist. Gemeinsam mit Ihnen erarbeiten wir eine Lösung, die Ihre Unternehmensziele bestmöglich unterstützt. <br><br> ", 
     impressumTitle: 'IMPRESSUM',
-    impressumText: `
-      Gesetzliche Vertretung: Wilmer Aderbert Florez Lopez <br>  
-      Calle 75 # 58-51 – 111211 Bogotá, Kolumbien<br>
-      wilmerkopernikus@gmail.com<br>
-      Registriert und rechtlich vertreten in Bogotá, Kolumbien.<br>
-      Umsatzsteuer (MwSt) ist auf internationale Transaktionen nicht anwendbar.
-    `,
-
+    impressumText: "Gesetzliche Vertretung: Wilmer Aderbert Florez Lopez <br>Calle 75 # 58-51 – 111211 Bogotá, Kolumbien<br>wilmerkopernikus@gmail.com<br>Registriert und rechtlich vertreten in Bogotá, Kolumbien.<br>Umsatzsteuer (MwSt) ist auf internationale Transaktionen nicht anwendbar.",
     formTitle: 'KONTAKT',
     formNameLabel: 'Ihr Name:',
     formEmailLabel: 'Ihre E-Mail:',
@@ -84,26 +60,21 @@ const languagesContent = {
 
   en: {
 
+    //menu
     menuIndex: `Home`,
     menuProjekte: `Projects`,
     menuCV: `Services`,
     menuKontakt: `Contact`,
 
+    //index
     indexTitle: `WELCOME TO A NEW <br> DIGITAL COSMOS!`,
-    wilmerIntroduction: `I am a web developer, graphic designer, and visual artist with international 
-    experience in Germany, China, and Colombia. In this portfolio, I present both professional and 
-    personal projects that reflect my approach: combining design, technology, and creativity to develop 
-    high-quality digital solutions.`,
-
+    wilmerIntroduction: "I am a web developer, graphic designer, and visual artist with international experience in Germany, China, and Colombia. In this portfolio, I present both professional and personal projects that reflect my approach: combining design, technology, and creativity to develop high-quality digital solutions.",
     projectTitle: `PROJECTS`,
     projectContent: `Discover a selection of project examples that show how I have supported clients through impactful web design and development.`,
     projectButton: `View my projects`,
-
     curriculumTitle: `CURRICULUM VITAE`,
-    cvContent: `Explore my resume in digital form and learn more about my professional experience, 
-    technical skills, and international background.`,
+    cvContent: "Explore my resume in digital form and learn more about my professional experience, technical skills, and international background.",
     curriculumButton: `View my CV`,
-
     contactTitle: `CONTACT`,
     contactContent: `Would you like to work with me? Whether you already have a clear vision or want to develop initial ideas together, feel free to get in touch — and let’s set your digital future in motion.`,
     contactButton: `Go to contact`,
@@ -111,15 +82,16 @@ const languagesContent = {
 
     //Projekte
     projekteTitle: "PROJECTS",
-
     joblabKunde: "<strong>Client</strong><br/>German Academic Exchange Service (DAAD)",
     joblabLeistungen: "<strong>What we did</strong><br/>Brand Design - Poster Design - Web Development - Website Design",
     joblabUebersicht: `<strong>Overview</strong><br/>In May and June 2024, the DAAD in China organized the project Job Lab in collaboration with the German Embassy, the Goethe-Institut, and other renowned institutions. Under the motto "With German into the Future," the fair offered career perspectives for Chinese students of German to support them on their path to the working world.`,
-    
-    blancecKunde:"",
-    blancecLeistungen:"",
-    blancecUebersicht:"",
-
+    blancecKunde:"<strong>Client</strong><br /> Blancec Laboratories S.A.S.",
+    blancecLeistungen:"<strong>What we did</strong><br /> Web Development & Design - Brand Design - Database - Process Automation",
+    blancecUebersicht:"<strong>Overview</strong><br />BLANCEC S.A.S is a laboratory from Colombia specialized in product testing. They evaluate products from various industries to ensure they meet the necessary regulations for commercialization in global markets, guaranteeing that they are high-quality, safe, and reliable.",
+    tarotTitle:"INTERACTIVE TAROT",
+    tarotKunde:"<strong>Client</strong><br /> Non-profit project",
+    tarotLeistungen:"<strong>What we did</strong><br /> Web Development & Design",
+    tarotUebersicht:"<strong>Overview</strong><br />Collaborative project with Professor Dioscórides Pérez, visual arts professor at the National University of Colombia, to develop an interactive tarot game. Using his original drawings, I am creating the digital program that translates his artistic and symbolic vision into an interactive experience.</p><br />",
     mehrInformation: "More Information",
     
     //Joblab
@@ -129,28 +101,11 @@ const languagesContent = {
 
     //Kontakt
     heroTitle: 'CONTACT',
-
     chooseUsTitle: 'MAKE US YOUR CHOICE!',
-    chooseUsText: `
-      Ready to take your business to the next level?
-      Having a website is not just about being online – it’s about opening new opportunities.
-      A well-designed website helps you gain customers, build trust, and present your services 24/7.
-      <br><br>
-      We help you create a website that is not only visually appealing, but also user-friendly
-      and search-engine optimized. Together with you, we develop a solution that supports your
-      business goals in the best possible way.
-      <br><br>
-    `,
+    chooseUsText: "Ready to take your business to the next level? Having a website is not just about being online – it’s about opening new opportunities. A well-designed website helps you gain customers, build trust, and present your services 24/7. <br><br> We help you create a website that is not only visually appealing, but also user-friendly and search-engine optimized. Together with you, we develop a solution that supports your business goals in the best possible way. <br><br> ",
     impressumTitle: 'LEGAL NOTICE',
-    impressumText: `
-      Legal representative: Wilmer Aderbert Florez Lopez <br>
-      Calle 75 # 58-51 – 111211 Bogotá, Colombia<br>
-      wilmerkopernikus@gmail.com<br>
-      Registered and legally represented in Bogotá, Colombia.<br>
-      VAT is not applicable to international transactions.
-    `,
-
-    formTitle: 'CONTACT',
+    impressumText: "Legal representative: Wilmer Aderbert Florez Lopez <br>Calle 75 # 58-51 – 111211 Bogotá, Colombia<br>wilmerkopernikus@gmail.com<br>Registered and legally represented in Bogotá, Colombia.<br>VAT is not applicable to international transactions.",
+    formTitle: 'CONTACT ME',
     formNameLabel: 'Your name:',
     formEmailLabel: 'Your email:',
     formMessageLabel: 'Your message:',
