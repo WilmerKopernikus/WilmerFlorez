0bf6f283dd6a9b26ca72776b06a97ee1c124951a
Wilmer Kopernikus <wilmerkopernikus@gmail.com>
2026-05-04 01:21:30 +0800

    add script for mobile in html files

---
+++ components/contact-shell-mobile.js
@@ -0,0 +1,41 @@
+class ContactShellMobile extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `  
+          <div class="card-body" id="contact-proportion-text">
+            <div class="right"></div>
+
+            <div class="left" id="center-services">
+              <div class="card-content-flex">
+                <h2 class="title" id="let-there-be-light" data-i18n="chooseUsTitle">ARBEITEN WIR ZUSAMMEN?</h2>
+                <p class="text-new low-light" id="center-text" data-i18n="">
+                  Sind Sie auf der Suche nach einem engagierten Webentwickler für
+                  Ihr Team oder Ihr nächstes Projekt?
+                </p><br>
+                <h2 class="title" id="let-there-be-light" data-i18n="formTitle">KONTAKTIEREN SIE MICH!</h2>
+                <form class="contact-form" name="contact" method="POST" data-netlify="true">
+                  <input type="hidden" name="form-name" value="contact">
+                  <p style="display:none;">
+                    <label>Don't fill this out if you're human: <input name="bot-field"></label>
+                  </p>
+                  <div class="form-group">
+                    <label for="name" id="low-light" data-i18n="formNameLabel">Ihr Name:</label>
+                    <input type="text" id="name" name="name" required />
+                  </div>
+                  <div class="form-group">
+                    <label for="email" id="low-light" data-i18n="formEmailLabel">Ihre E-Mail:</label>
+                    <input type="email" id="email" name="email" required />
+                  </div>
+                  <div class="form-group">
+                    <label for="message" id="low-light" data-i18n="formMessageLabel">Ihre Nachricht:</label>
+                    <textarea id="message" name="message" required></textarea>
+                  </div>
+                  <button type="submit" class="send-button" id="low-light" data-i18n="formSubmit">Senden</button>
+                </form>
+              </div>
+            </div>
+          </div>
+    `;
+  }
+}
+
+customElements.define('contact-shell-mobile', ContactShellMobile);
---
+++ components/impressum-shell-mobile.js
@@ -0,0 +1,79 @@
+class ImpressumShellMobile extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `  
+          <div class="card-body" id="contact-proportion-text">
+
+            <div class="left" id="center-services-mobile"></div>
+
+            <div class="right" id="center-services">
+              <div class="card-content-flex">
+                <h2 class="title" id="let-there-be-light" data-i18n="footerTitle">KREATIV UND INNOVATIV</h2>
+
+                <div class="service-item active">
+                  <div class="service-trigger text-new justify">
+                    Wilmer Aderbert Florez López
+                  </div>
+                  <div class="service-description text justify" data-i18n="footerDescription">
+                    Web Developer - UI/UX Designer - Creative Coder<br>
+                    Creating performant, user-focused web experiences<br>
+                    Based in Germany · Working internationally<br>
+                    wilmerkopernikus@gmail.com <br>
+                    (+49)1771 827690 - (+86)185 2006 0442
+
+                  </div>
+                </div>
+                <div class="service-item">
+                  <div class="service-trigger text-new justify" data-i18n="footerNavigation">
+                    Navigation
+                  </div>
+                  <div class="service-description text justify">
+                    <a href="index.html" class="text-link" data-i18n="menuIndex">Startseite</a><br>
+                    <a href="kundenprojekte.html" class="text-link" data-i18n="menuProjekte">Kundenprojekte</a><br>
+                    <a href="meine_projekte.html" class="text-link" data-i18n="menuEigeneProjekte">Meine
+                      Projekte</a><br>
+                    <a href="creative_coding.html" class="text-link" data-i18n="menuCreativeCoding">Creative
+                      Coding</a><br>
+                    <a href="about.html" class="text-link" data-i18n="menuUeberMich">Über Mich</a><br>
+                    <a href="kontakt.html" class="text-link" data-i18n="menuKontakt">Kontakt</a><br>
+                  </div>
+                </div>
+                <div class="service-item">
+                  <div class="service-trigger text-new justify">
+                    Github
+                  </div>
+                  <div class="service-description text justify">
+                    <a href="https://github.com/wilmerkopernikus" class="text-link"
+                      target="_blank">https://github.com/wilmerkopernikus</a><br>
+                  </div>
+                </div>
+                <div class="service-item">
+                  <div class="service-trigger text-new justify" data-i18n="impressumTitle">
+                    Impressum
+                  </div>
+                  <div class="service-description text justify" data-i18n="impressumContent">
+                    Wilmer Aderbert Florez López<br>
+                    Am Stumpenhof 14 - 73207 Plochingen, Deutschland <br>
+                    wilmerkopernikus@gmail.com<br>
+                    (+49)1771 827690 - (+86)185 2006 0442<br>
+                    Responsible for content according to §55 Abs. 2 RStV
+                  </div>
+                </div>
+
+                <p class="text-new" id="low-light" data-i18n="derechos">© 2026 Wilmer Florez. Alle Rechte vorbehalten.
+                </p><br>
+                <video id="services-animation-mobile" autoplay loop muted playsinline preload="metadata" loading="lazy"
+                  poster="imagenes/videos/services_04.jpg" class="white-shadow">
+                  <source src="imagenes/videos/services_04_compressed.mp4" type="video/mp4" />
+                  Your browser doesn't support video reproductions.
+                </video>
+
+              </div>
+
+            </div>
+
+          </div>
+    `;
+  }
+}
+
+customElements.define('impressum-shell-mobile', ImpressumShellMobile);
